0044d5283560f322e5fba4f2c4c273755dc58caf
Bwaaba7 <43949738+Bwaaba7@users.noreply.github.com>
2018-12-10 18:17:51 +0100

    AJOUT DES LEVELs

--- QuantumGate/Assets/EasyCampaign.cs
+++ QuantumGate/Assets/EasyCampaign.cs
@@ -8,16 +8,6 @@ public class EasyCampaign : MonoBehaviour {
 
 	public void OnClick()
 	{
-		List<QCS.Circuit> circuits = new List<QCS.Circuit>
-		{
-			new QCS.Circuit(10, 1)
-		};
-
-		GameMode.nameGame = "";
-		GameMode.circuits = circuits;
-		GameMode.gates = new List<QCS.Gate>() { QCS.Gate.NOT, QCS.Gate.CONTROL};
-		GameMode.customGates = new List<QCS.Gate>();
-
-		SceneManager.LoadScene("SandBox 1", LoadSceneMode.Single);
+		SceneManager.LoadScene("ChoixLevel", LoadSceneMode.Single);
 	}
 }
